a27d6bbba5935cdba3db2338021246eca0929bb3
Daria Ganchukova <umi194@gmail.com>
2024-06-02 20:16:19 +0200

    Solution update

--- app/main.py
+++ app/main.py
@@ -1,26 +1,3 @@
-import functools
-from typing import Callable
-
-
-def _if_can_wash(func: Callable) -> Callable:
-    @functools.wraps(func)
-    def wrapper(self: "CarWashStation", *args, **kwargs) -> None:
-        if isinstance(args[0], Car):
-            if self.clean_power > args[0].clean_mark:
-                return func(self, *args, **kwargs)
-        elif isinstance(args[0], list):
-            cars = args[0]
-            filtered_cars = [
-                car for car in cars if car.clean_mark < self.clean_power
-            ]
-            return func(self,
-                        filtered_cars,
-                        *args[1:], **kwargs)
-        return None
-
-    return wrapper
-
-
 class Car:
     def __init__(self,
                  comfort_class: int,
@@ -42,7 +19,6 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    @_if_can_wash
     def serve_cars(self, cars: list[Car]) -> float:
         income = 0.0
         for car in cars:
@@ -50,14 +26,12 @@ class CarWashStation:
             self.wash_single_car(car)
         return round(income, 1)
 
-    @_if_can_wash
     def calculate_washing_price(self, car: Car) -> float:
         delta_car_wash = self.clean_power - car.clean_mark
         price = (car.comfort_class * delta_car_wash
                  * self.average_rating / self.distance_from_city_center)
-        return round(price, 1)
+        return round(price, 1) if self.clean_power > car.clean_mark else 0.0
 
-    @_if_can_wash
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
